06a0f33153e88858df3a700cc2731487869b6958
Cipherclone <cipherjamey@gmail.com>
2022-08-24 21:36:07 -0500

    added hello world program and section 1 program

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * James Sparkman
+ * ITSE 1430 2022 Fall
+ * Lab 1
+ * This is also the testing playground for a while
+ */
+
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name); // ah yes, the funny comments begin here
+            
+        }
+    }
+}
