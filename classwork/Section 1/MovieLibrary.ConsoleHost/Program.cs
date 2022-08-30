f35185d5bd0fd3bb2e14e50e0ceaa715ca9d1e19
Cipherclone <cipherjamey@gmail.com>
2022-08-29 21:05:34 -0500

    started MovieLibrary project

---
+++ classwork/Section 1/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,15 @@
+﻿// movie definition
+string title = "";
+string description = "";
+int runLength = 0; // in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+Console.WriteLine("Enter a title: ");
+Console.WriteLine("Enter an optional desctription: ");
+Console.WriteLine("Enter a run length (in minutes): ");
+Console.WriteLine("Enter a release year: ");
+Console.WriteLine("Enter a MPAA rating: ");
+Console.WriteLine("Is this a classic? ");
+
