4bbd374fb233b3b8be60c42088874fb105a6948c
Cipherclone <cipherjamey@gmail.com>
2022-08-31 21:45:59 -0500

    added to movie library

--- classwork/Section 1/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/Section 1/MovieLibrary.ConsoleHost/Program.cs
@@ -6,10 +6,34 @@ int releaseYear = 1900;
 string rating = "";
 bool isClassic = false;
 
-Console.WriteLine("Enter a title: ");
-Console.WriteLine("Enter an optional desctription: ");
-Console.WriteLine("Enter a run length (in minutes): ");
-Console.WriteLine("Enter a release year: ");
-Console.WriteLine("Enter a MPAA rating: ");
+
+title = ReadString("Enter a title: ");
+
+description = ReadString("Enter an optional desctription: ");
+runLength = ReadInt32("Enter a run length (in minutes): ");
+
+releaseYear = ReadInt32("Enter a release year: ");
+rating = ReadString("Enter a MPAA rating: ");
 Console.WriteLine("Is this a classic? ");
 
+string ReadString(string message)
+{
+    Console.WriteLine(message);
+
+    string value = Console.ReadLine();
+
+    return value;
+    
+}
+
+int ReadInt32(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+
+    if (Int32.TryParse(value, out int result))
+        return result;
+    else
+        return -1;
+    
+}
