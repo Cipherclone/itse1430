aeaf3abe9decf08777af999d0462b29c22677ba7
Cipherclone <cipherjamey@gmail.com>
2022-09-07 21:27:03 -0500

    added fixes to MovieLibrary

--- classwork/Section 1/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/Section 1/MovieLibrary.ConsoleHost/Program.cs
@@ -5,35 +5,80 @@ int runLength = 0; // in minutes
 int releaseYear = 1900;
 string rating = "";
 bool isClassic = false;
+AddMovie();
+
+bool ReadBoolean( string message)
+{
+    Console.Write(message);
+
+    // looking for y/n
+    ConsoleKeyInfo key = Console.ReadKey();
+
+    if (key.Key == ConsoleKey.Y)
+        return true;
+    else if (key.Key == ConsoleKey.N)
+        return false;
+    
+    //TODO error
+    return false;
+}
 
 
-title = ReadString("Enter a title: ");
-
-description = ReadString("Enter an optional desctription: ");
-runLength = ReadInt32("Enter a run length (in minutes): ");
-
-releaseYear = ReadInt32("Enter a release year: ");
-rating = ReadString("Enter a MPAA rating: ");
-Console.WriteLine("Is this a classic? ");
-
-string ReadString(string message)
+string ReadString(string message, bool required)
 {
     Console.WriteLine(message);
 
-    string value = Console.ReadLine();
+    while (true)
+    {
 
-    return value;
+        string value = Console.ReadLine();
+
+
+        // check the boolean
+
+        // TODO value empty and required
+        if (value != "" || !required)
+            return value;
+
+        Console.WriteLine("Value is required");
+
+    };
+}
+
+int ReadInt32(string message, int mininmumValue, int maximumValue)
+{
+    Console.Write(message);
+
+    do
+    {
+        string value = Console.ReadLine();
+
+        if (Int32.TryParse(value, out int result))
+        {
+            if (result >= mininmumValue && result <= maximumValue)
+            return result;
+
+        }
+
+        Console.WriteLine("Value must be between " + mininmumValue + " and " + maximumValue);
+        //TODO loop
+
+        //breaks exit loop
+        //continue iterates
+
+    } while (true);
+
+
+    
     
 }
 
-int ReadInt32(string message)
+void AddMovie ()
 {
-    Console.Write(message);
-    string value = Console.ReadLine();
-
-    if (Int32.TryParse(value, out int result))
-        return result;
-    else
-        return -1;
-    
+    title = ReadString("Enter a title: ", true);
+    description = ReadString("Enter an optional desctription: ", false);
+    runLength = ReadInt32("Enter a run length (in minutes): ", 0, 300);
+    releaseYear = ReadInt32("Enter a release year: ", 1900, 2100);
+    rating = ReadString("Enter a MPAA rating: ", true);
+    isClassic = ReadBoolean("Is this a classic? ");
 }
