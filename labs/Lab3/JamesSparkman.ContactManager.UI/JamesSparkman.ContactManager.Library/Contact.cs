ba7bd8303786af9abacf50862bb04b6d38393614
Cipherclone <cipherjamey@gmail.com>
2022-10-30 01:24:19 -0500

    added library

---
+++ labs/Lab3/JamesSparkman.ContactManager.UI/JamesSparkman.ContactManager.Library/Contact.cs
@@ -0,0 +1,18 @@
+﻿namespace JamesSparkman.ContactManager.Library
+{
+    public class Contact
+    {
+        // TODO add constructors
+        public Contact ()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+        public bool IsFavorite { get; set; } = false;
+
+    }
+}
