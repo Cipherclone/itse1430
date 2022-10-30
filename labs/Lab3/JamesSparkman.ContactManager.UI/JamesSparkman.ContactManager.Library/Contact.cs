5fbc16589314593a12c9a1ae0f440978671dfbbb
Cipherclone <cipherjamey@gmail.com>
2022-10-30 01:37:23 -0500

    working on Contact.cs

--- labs/Lab3/JamesSparkman.ContactManager.UI/JamesSparkman.ContactManager.Library/Contact.cs
+++ labs/Lab3/JamesSparkman.ContactManager.UI/JamesSparkman.ContactManager.Library/Contact.cs
@@ -1,5 +1,6 @@
-﻿namespace JamesSparkman.ContactManager.Library
+﻿namespace JamesSparkman.ContactManager.Library // im on story 2 rn
 {
+    /// <summary>Represents a Contact</summary>
     public class Contact
     {
         // TODO add constructors
@@ -8,11 +9,45 @@
 
         }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Notes { get; set; }
+        /// <summary>Optional First Name</summary>
+        public string FirstName 
+        {
+            get { return _firstName ?? ""; }
+            set { _firstName = value?.Trim() ?? ""; }
+        }
+        private string _firstName;
+        
+        /// <summary>Required Last Name</summary>
+        public string LastName 
+        {
+            get { return _lastName ?? ""; }
+            set { _lastName = value?.Trim() ?? ""; } 
+        }
+        private string _lastName;
+
+        /// <summary>Required and Unique E-Mail Address</summary>
+        public string Email 
+        {
+            get { return _email ?? ""; } 
+            set { _email = value?.Trim() ?? ""; }
+        }
+        private string _email;
+        
+        /// <summary>Optional Notes</summary>
+        public string Notes 
+        { 
+            get { return _notes ?? ""; }
+            set { _notes = value ?.Trim() ?? ""; } 
+        }
+        private string _notes;
+        
+        /// <summary>Bool Representing if a Contact is listed as a Favorite</summary>
         public bool IsFavorite { get; set; } = false;
 
+
+        bool IsValidEmail(string source)
+        {
+            return true; // TODO this function
+        }
     }
 }
