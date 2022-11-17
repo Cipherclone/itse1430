5bfdc954b28cb93cc365cb93cccf2d22e34439c0
Cipherclone <cipherjamey@gmail.com>
2022-11-17 14:11:42 -0600

    database workd

--- classwork/MovieLibrary/MovieLibrary.Sql/SqlMovieDatabase.cs
+++ classwork/MovieLibrary/MovieLibrary.Sql/SqlMovieDatabase.cs
@@ -1,12 +1,65 @@
-﻿namespace MovieLibrary.Sql
+﻿using System.Data.SqlClient;
+
+namespace MovieLibrary.Sql
 {
     public class SqlMovieDatabase : MovieDatabase
     {
-        protected override Movie AddCore ( Movie movie ) => throw new NotImplementedException();
-        protected override Movie FindByTitle ( string title ) => throw new NotImplementedException();
-        protected override IEnumerable<Movie> GetAllCore () => throw new NotImplementedException();
-        protected override Movie GetCore ( int id ) => throw new NotImplementedException();
+        public SqlMovieDatabase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        protected override Movie AddCore ( Movie movie )
+        {
+            SqlConnection conn = null;
+
+            try
+            {
+                conn = OpenConnection();
+
+                throw new NotImplementedException();
+            } finally
+            {
+                // close connect
+                conn.Close();
+            }
+            
+        }
+        protected override Movie FindByTitle ( string title )
+        {
+            var conn = OpenConnection();
+
+            // close connect
+            conn.Close();
+            // Find by ID
+            return null;
+        }
+        protected override IEnumerable<Movie> GetAllCore () {
+            var conn = OpenConnection();
+
+            conn.Close();
+
+            return Enumerable.Empty<Movie>();
+        }
+        protected override Movie GetCore ( int id )
+        {
+            var conn = OpenConnection();
+
+            // close connect
+            conn.Close();
+            // Find by ID
+            return null;
+        }
         protected override void RemoveCore ( int id ) => throw new NotImplementedException();
         protected override void UpdateCore ( int id, Movie movie ) => throw new NotImplementedException();
+
+        private SqlConnection OpenConnection ()
+        {
+            var conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            return conn;
+        }
+
+        private readonly string _connectionString;
     }
 }
