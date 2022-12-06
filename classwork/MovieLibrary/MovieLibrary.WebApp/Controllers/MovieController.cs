db059b22bd061375617d2e17bfa1942688054024
Cipherclone <cipherjamey@gmail.com>
2022-12-06 11:25:00 -0600

    controllers

---
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿    using Microsoft.AspNetCore.Mvc;
+
+namespace MovieLibrary.WebApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        public MovieController ( IConfiguration configuration )
+        {
+            var connString = configuration.GetConnectionString("AppDatabase");
+
+            _database = new Sql.SqlMovieDatabase (connString);
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Index ()
+        {
+            var movies =  _database.GetAll();
+
+            return View(movies);
+        }
+
+        public ActionResult<Movie> Detail ( int id )
+        {
+            var movie = _database.Get(id);
+
+            return View(movie);
+
+        }
+
+        private readonly Sql.SqlMovieDatabase _database;
+    }
+}
