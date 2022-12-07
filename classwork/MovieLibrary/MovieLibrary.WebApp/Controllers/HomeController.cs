ec9d6de2822ae9eadffe22fe4b7009f4caa92cd0
Cipherclone <cipherjamey@gmail.com>
2022-12-07 16:31:13 -0600

    updating MovieLibrary

--- classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/HomeController.cs
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 using MovieLibrary.WebApp.Models;
+
+using System.Diagnostics;
 
 namespace MovieLibrary.WebApp.Controllers
 {
--- classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MovieController.cs
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MovieController.cs
@@ -1,30 +1,35 @@
-﻿    using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MovieLibrary.WebApp.Controllers
 {
+    //[Route("movies")]
     public class MovieController : Controller
     {
         public MovieController ( IConfiguration configuration )
         {
             var connString = configuration.GetConnectionString("AppDatabase");
 
-            _database = new Sql.SqlMovieDatabase (connString);
+            _database = new Sql.SqlMovieDatabase(connString);
         }
 
+        //Action = public methods on controllers
         [HttpGet]
         public ActionResult<IEnumerable<Movie>> Index ()
         {
-            var movies =  _database.GetAll();
+            var movies = _database.GetAll();
 
+            //return Ok(movies);
             return View(movies);
         }
 
-        public ActionResult<Movie> Detail ( int id )
+        [HttpGet]
+        public ActionResult<Movie> Details ( int id )
         {
             var movie = _database.Get(id);
+            if (movie == null)
+                return NotFound();
 
             return View(movie);
-
         }
 
         private readonly Sql.SqlMovieDatabase _database;
